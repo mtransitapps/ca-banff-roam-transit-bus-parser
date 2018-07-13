9520c40ad85ca05662d3c822fd5f870cd05f6e34
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-13 10:01:01 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_banff_roam_transit_bus/BanffRoamTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_banff_roam_transit_bus/BanffRoamTransitBusAgencyTools.java
@@ -54,6 +54,11 @@ public class BanffRoamTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
@@ -248,7 +253,7 @@ public class BanffRoamTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		System.out.printf("\n%s: Unexpected compare early route!\n", routeId);
 		System.exit(-1);
@@ -268,7 +273,7 @@ public class BanffRoamTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		System.out.printf("\n%s: Unexpected split trip stop route!\n", mRoute.getId());
 		System.exit(-1);
